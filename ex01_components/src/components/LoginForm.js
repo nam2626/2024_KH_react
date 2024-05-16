fe7c53d325c9d11a1e7dc337fecefed8f03005e9
elzenaro <nam2626@naver.com>
2024-05-16 13:50:41 +0900

    로그인 폼.

--- ex01_components/src/components/LoginForm.js
+++ ex01_components/src/components/LoginForm.js
@@ -1,10 +1,15 @@
 import './../css/login.css';
 function LoginForm(){
+  const loginClick = () => {
+    // alert('로그인 버튼 클릭');
+    // 아이디와 비밀번호를 입력한 input 태그 접근
+
+  }
   return (
     <div className='loginForm'>
-      <input type='text' id='name'></input>
+      <input type='text' id='id'></input>
       <input type='password' id='pass'/>
-      <button className='btn_login'>로그인</button>
+      <button className='btn_login' onClick={loginClick}>로그인</button>
     </div>
   );
 }
