4c0bc4c118d86c6a55f7b12a80831b410f3df7a3
elzenaro <nam2626@naver.com>
2024-05-16 16:01:36 +0900

    텍스트 상자

--- ex01_components/src/components/TextBox.js
+++ ex01_components/src/components/TextBox.js
@@ -6,13 +6,26 @@ export default () => {
     txt.value = '';
   }
   const txtBox = (e) => {
-    console.log(e);
+    // console.log(e);
+    // 텍스트 상자에 데이터를 입력했을때
+    // 입력한 텍스트가 6글자 이상이고, 숫자로만 입력되지 않았을 때
+    // 데이터를 정상적으로 입력하셨습니다.
+    // 만약 숫자만 입력하거나, 텍스트가 6글자 미만이면, 
+    // 데이터를 비정상적으로 입력하였습니다. 
+    // txt_area에 출력
+    let txt = e.target.value;
+    //console.log(txt);
+    if(txt.length >= 6 && isNaN(txt)){
+      document.querySelector('.txt_area').innerHTML = '정상적으로 입력하셨습니다.';
+    }else{
+      document.querySelector('.txt_area').innerHTML = '데이터를 비정상적으로 입력하였습니다.';
+    }
   }
   return (
     <div>
       <input type='text' id='txt' placeholder="텍스트를 입력하세요" onChange={txtBox}/>
       <button onClick={txtClear}>초기화</button>
-      <div class='txt_area'></div>
+      <div className='txt_area'></div>
     </div>
   );
 }
