86b48ac043b59478ebeffb9c4c123d568dda8138
elzenaro <nam2626@naver.com>
2024-05-17 14:37:17 +0900

    ref range

--- ex03_hook/src/components/RangeComponent.js
+++ ex03_hook/src/components/RangeComponent.js
@@ -1,9 +1,16 @@
+import { useRef,useState } from "react"
+
 export default function RangeComponent() {
   //range 값이 바뀌면, 값 옆에 range 값을 최신화. useState, useRef
+  let range = useRef();
+  let [val, setVal] = useState(0);
+  const dragRange = () => {
+    setVal(range.current.value);
+  }
   return (
     <>
-      <input type="range" min={0} max={100} step={1}></input> 
-      <span> 값 : {}</span>
+      <input type="range" min={0} max={100} step={1} ref={range} onChange={dragRange}></input> 
+      <span> 값 : {val}</span>
     </>
   )
 }
