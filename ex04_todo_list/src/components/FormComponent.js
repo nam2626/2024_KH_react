4a22f656667621da3264d1d75c34f5c51d9496cd
elzenaro <nam2626@naver.com>
2024-05-17 16:37:06 +0900

    FormComponent

--- ex04_todo_list/src/components/FormComponent.js
+++ ex04_todo_list/src/components/FormComponent.js
@@ -9,10 +9,14 @@ export default () => {
     setList([...list, item]);
     console.log(list);
   }
+  const deleteItem = (n) => {
+    setList(list.filter((obj,idx) => obj.id !== n));
+  }
   return(
     <>
       <input type="text" ref={txt}/>
       <button onClick={addItem}>추가</button>
+      <hr></hr>
     </>
   ); 
 }
