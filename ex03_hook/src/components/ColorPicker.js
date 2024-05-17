075084307518bb77c247f545fc87806fd3bb7cf6
elzenaro <nam2626@naver.com>
2024-05-17 15:59:13 +0900

    state, ref

--- ex03_hook/src/components/ColorPicker.js
+++ ex03_hook/src/components/ColorPicker.js
@@ -4,6 +4,7 @@ export default function ColorPicker() {
   const red = useRef();
   const green = useRef();
   const blue = useRef();
+  const colorPicker = useRef();
 
   const changeRange = () => {
     // console.log(red.current.value, typeof red.current.value);
@@ -16,9 +17,19 @@ export default function ColorPicker() {
     if(g.length == 1) g = '0' + g;
     if(b.length == 1) b = '0' + b;
     let color = '#' + r + g + b;
-
     console.log(color);
 
+    colorPicker.current.value = color;
+
+  }
+
+  const handlerColorPicker = () => {
+    console.log(colorPicker.current.value.substr(1,2));
+    console.log(colorPicker.current.value.substr(3,2));
+    console.log(colorPicker.current.value.substr(5,2));
+    red.current.value = parseInt(colorPicker.current.value.substr(1,2),16);
+    green.current.value = parseInt(colorPicker.current.value.substr(3,2),16);
+    blue.current.value = parseInt(colorPicker.current.value.substr(5,2),16);
   }
 
   return (
@@ -26,7 +37,7 @@ export default function ColorPicker() {
       r : <input type="range" ref={red} defaultValue={0} onChange={changeRange} min={0} max={255} step={1}/><br/>
       g : <input type="range" ref={green} defaultValue={0} onChange={changeRange} min={0} max={255} step={1}/><br/>
       b : <input type="range" ref={blue} defaultValue={0} onChange={changeRange} min={0} max={255} step={1}/><br/>
-      <input type="color" defaultValue="#000000" ref={}/>
+      <input type="color" defaultValue="#000000" ref={colorPicker} onChange={handlerColorPicker}/>
     </>
   );
 }
