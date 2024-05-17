bdf5bcf6d4e9ed34dedfdd17a19e1e828afcf135
elzenaro <nam2626@naver.com>
2024-05-17 17:15:11 +0900

    css 적용

--- ex04_todo_list/src/components/FormComponent.js
+++ ex04_todo_list/src/components/FormComponent.js
@@ -1,12 +1,12 @@
 import { useRef, useState } from "react";
 import ItemComponent from "./ItemComponent";
-
+import './../css/form.css';
 export default () => {
   let [list, setList] = useState([]);
   let count = useRef(0)
   const txt = useRef();
   const addItem = () => {
-    let item = {id:++count.current, val : txt.current.value};
+    let item = {id:++count.current, val : txt.current.value, done: false};
     setList([...list, item]);
     console.log(list);
   }
@@ -15,10 +15,19 @@ export default () => {
   }
   return(
     <>
-      <input type="text" ref={txt}/>
-      <button onClick={addItem}>추가</button>
+    <div className="form-container">
+      <input type="text" ref={txt} className="input"/>
+      <button onClick={addItem} className="btn add">추가</button>
+    </div>
       <hr></hr>
-      {list.map((obj) => <ItemComponent key={obj.id} id={obj.id} val={obj.val} deleteItem={deleteItem} />)}
+      <table className="area">
+        <tr>
+          <th>순번</th>
+          <th>내용</th>
+          <th>작업</th>
+        </tr>
+      {list.map((obj) => <ItemComponent key={obj.id} id={obj.id} val={obj.val} done={obj.done} deleteItem={deleteItem} />)}
+      </table>
     </>
   ); 
 }
--- ex04_todo_list/src/components/ItemComponent.js
+++ ex04_todo_list/src/components/ItemComponent.js
@@ -1,9 +1,14 @@
 export default function ItemComponent(props) {
-  let {id, val, deleteItem} = props;
+  let {id, val, done, deleteItem} = props;
   return (
-    <p>
-      {id} / {val} / 
-      <button onClick={() => deleteItem(id)}>삭제</button>
-    </p>
+    <tr>
+      <td>{id}</td>
+      <td className={done ? 'done' : ''}>{val}</td>
+      <td>
+        <button className="btn finish">작업 완료</button>
+        <button onClick={() => deleteItem(id)} className="btn delete">삭제</button>
+      </td>
+        
+    </tr>
   );
 }
