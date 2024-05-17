0d091ccc8bb1aec2db78b6a64179112d083931f6
elzenaro <nam2626@naver.com>
2024-05-17 17:50:36 +0900

    문제 답안

--- ex04_todo_list/src/components/FormComponent.js
+++ ex04_todo_list/src/components/FormComponent.js
@@ -1,9 +1,65 @@
 import { useRef, useState } from "react";
 import ItemComponent from "./ItemComponent";
 import './../css/form.css';
+//초기 랜덤 데이터
+function makeItem(){
+  const items = [
+    {
+        "id": 1,
+        "val": "빨래하기",
+        "done": false
+    },
+    {
+        "id": 2,
+        "val": "장보기",
+        "done": false
+    },
+    {
+        "id": 3,
+        "val": "청소하기",
+        "done": false
+    },
+    {
+        "id": 4,
+        "val": "요리하기",
+        "done": false
+    },
+    {
+        "id": 5,
+        "val": "운동하기",
+        "done": false
+    },
+    {
+        "id": 6,
+        "val": "책 읽기",
+        "done": false
+    },
+    {
+        "id": 7,
+        "val": "이메일 확인",
+        "done": false
+    },
+    {
+        "id": 8,
+        "val": "프로젝트 작업",
+        "done": false
+    },
+    {
+        "id": 9,
+        "val": "미팅 준비",
+        "done": false
+    },
+    {
+        "id": 10,
+        "val": "전화하기",
+        "done": false
+    }
+];
+  return items;
+}
 export default () => {
-  let [list, setList] = useState([]);
-  let count = useRef(0)
+  let [list, setList] = useState(makeItem());
+  let count = useRef(10)
   const txt = useRef();
   const addItem = () => {
     let item = {id:++count.current, val : txt.current.value, done: false};
@@ -12,6 +68,14 @@ export default () => {
   }
   const deleteItem = (n) => {
     setList(list.filter((obj,idx) => obj.id !== n));
+  }
+  const updateItem = (n) => {
+    setList(list.map((obj) => {
+      if(obj.id === n){
+        obj.done = true;
+      }
+      return obj;
+    }));
   }
   return(
     <>
@@ -23,10 +87,10 @@ export default () => {
       <table className="area">
         <tr>
           <th>순번</th>
-          <th>내용</th>
+          <th className="content">내용</th>
           <th>작업</th>
         </tr>
-      {list.map((obj) => <ItemComponent key={obj.id} id={obj.id} val={obj.val} done={obj.done} deleteItem={deleteItem} />)}
+      {list.map((obj) => <ItemComponent key={obj.id} id={obj.id} val={obj.val} done={obj.done} deleteItem={deleteItem} updateItem={updateItem}/>)}
       </table>
     </>
   ); 
--- ex04_todo_list/src/components/ItemComponent.js
+++ ex04_todo_list/src/components/ItemComponent.js
@@ -1,11 +1,11 @@
 export default function ItemComponent(props) {
-  let {id, val, done, deleteItem} = props;
+  let {id, val, done, deleteItem, updateItem} = props;
   return (
     <tr>
       <td>{id}</td>
       <td className={done ? 'done' : ''}>{val}</td>
       <td>
-        <button className="btn finish">작업 완료</button>
+        <button className="btn finish" onClick={() => updateItem(id)}>작업 완료</button>
         <button onClick={() => deleteItem(id)} className="btn delete">삭제</button>
       </td>
         
