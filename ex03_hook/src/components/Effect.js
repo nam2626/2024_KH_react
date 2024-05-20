98e0afd9ee89afd911ab27b79193d9f680b2eee9
elzenaro <nam2626@naver.com>
2024-05-20 10:20:06 +0900

    effect

--- ex03_hook/src/components/Effect.js
+++ ex03_hook/src/components/Effect.js
@@ -7,12 +7,15 @@ export default function Effect() {
   const handlerClick = () =>{
     setCounter(counter + 1);
   }
+  //[] 없음 => 리렌더링 될때마다 재실행이 된다.
+  //[] => 빈 배열, 최초로 로드될 때 딱 한번만 실행된다.
+  //[상태변수,...] => 지정한 상태 변수가 변경 될때만 새롭게 등록된다.
   useEffect( () => {
     setTimeout(() => {
       setToday(new Date());
       console.log('Effect 렌더링 : ',today.toLocaleString());
     },3000);
-  },[]);
+  },[counter]);
 
   return (
     <div>
