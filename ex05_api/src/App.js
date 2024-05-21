f43ea2c17a9276955d73c9d59879bf58ea090038
elzenaro <nam2626@naver.com>
2024-05-21 12:20:54 +0900

    kakaomap

--- ex05_api/src/App.js
+++ ex05_api/src/App.js
@@ -1,27 +1,9 @@
-import {useEffect} from 'react';
-const {kakao} = window;
+import React from 'react';
+import KakaoMap from "./components/KakaoMap";
 function App() {
-  const style = {
-    width : '500px',
-    height : '400px',
-    border: '1px solid black',
-    margin : '0 auto'
-  }
-  //script 태그로 라이브러리 읽으면 window 객체에 등록
-  // console.log(window);
-  useEffect(() => {
-    var container = document.getElementById('map');
-    var options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-    };
-    
-    var map = new kakao.maps.Map(container, options);
-  },[]);
-
   return (
     <div className="App">
-      	<div id="map" style={style}></div>
+      <KakaoMap/>      	
     </div>
   );
 }
