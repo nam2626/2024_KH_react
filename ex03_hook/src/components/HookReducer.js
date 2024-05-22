4b4105e7c2cf08391c064ca0116651bf2dd1c331
elzenaro <nam2626@naver.com>
2024-05-22 11:22:06 +0900

    reducer

--- ex03_hook/src/components/HookReducer.js
+++ ex03_hook/src/components/HookReducer.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useReducer } from "react";
+import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
 
 const reducer = (state, action) => {
   switch(action.type){
@@ -9,6 +9,8 @@ const reducer = (state, action) => {
       return {...state, str : action.payload};
     case 'changeToday':
       return {...state, today : new Date()};
+    case 'addNumber':
+      return {...state, list : [...state.list, Number(action.payload)]};
   }
 }
 export default function HookReducer() {
@@ -33,6 +35,19 @@ export default function HookReducer() {
       dispatch({type : 'changeToday'});
     },3000);
   },[data.today]);
+  const txtNum = useRef();
+  const addNumber = () => {
+    dispatch({type:'addNumber', payload : txtNum.current.value});
+  }
+  const getAverage = (arr) => {
+    //배열에 있는 내용 평균 리턴
+    if(arr.length == 0) return 0;
+    return arr.reduce((pre,cur) => pre + cur,0) / arr.length;
+  }
+  
+  const getAverageMemo = useMemo(() => {
+    return getAverage(data.list);
+  },[data.list]);
 
   return (
     <div>
@@ -42,6 +57,8 @@ export default function HookReducer() {
       <input type="text" name="str" onChange={changeString}/>
       Str : {data.str}<br></br>
       Date : {data.today.toLocaleString()}<br/>
+      Avg :  {data.list.join(',')} / {getAverageMemo}<br/>
+      <input type="text" ref={txtNum}/><button onClick={addNumber}>숫자 추가</button>
     </div>
 
   );
