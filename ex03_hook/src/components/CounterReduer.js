ceca5659e68f410df3f78662799f19b80eaa582e
elzenaro <nam2626@naver.com>
2024-05-22 10:00:50 +0900

    reducer

---
+++ ex03_hook/src/components/CounterReduer.js
@@ -0,0 +1,24 @@
+import React, { useReducer } from 'react';
+//const [변수명, dispatch] = useReducer(함수명,{키값:값,....});
+//state에는 상태값을 받음, action 해야될 일
+function reducer(state, action) {
+  switch(action.type){
+    case 'INCREMENT':
+      return {...state, value : state.value + 1};
+    case 'DECREMENT':
+      return {...state, value : state.value - 1};
+    default:
+      return state;
+  }
+}
+
+export default function CounterReducer() {
+  const [count,dispatch] = useReducer(reducer, {value : 0});
+  return (
+    <div>
+      <h2>Count : {count.value}</h2>
+      <button>증가</button>
+      <button>감소</button>
+    </div>
+  );
+}
