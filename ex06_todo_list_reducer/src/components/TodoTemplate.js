7d19b4ba327ea958854d12ea7b2bc8c2683f5c74
elzenaro <nam2626@naver.com>
2024-05-23 09:53:09 +0900

    컴포넌트 생성

---
+++ ex06_todo_list_reducer/src/components/TodoTemplate.js
@@ -0,0 +1,12 @@
+import FormComponent from "./FormComponent";
+
+export default function TodoTemplate() {
+
+  return (
+    <div>
+      <FormComponent/>
+      <hr></hr>
+      
+    </div>
+  );
+}
