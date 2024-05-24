1be6a5324c3609cacf77ad87a9031c7b99d9aa1f
elzenaro <nam2626@naver.com>
2024-05-24 14:25:50 +0900

    route

--- router_ex/src/components/Data.js
+++ router_ex/src/components/Data.js
@@ -3,12 +3,16 @@ import { useLocation } from "react-router-dom";
 export default function Data() {
   const location = useLocation();
   const {search} = useLocation();
+  const queryString = new URLSearchParams(search);
+
+  const name = queryString.get('name');
+  const age = queryString.get('age');
 
   console.log(location);
   return (
     <div>
       <h2>Data - QueryString</h2>
-      <p></p>
+      <p>{name} / {age}</p>
     </div>
   );
 }
