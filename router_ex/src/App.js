ec7fb5965956583ad185d56338c8973970874cce
elzenaro <nam2626@naver.com>
2024-05-24 13:48:51 +0900

    route

--- router_ex/src/App.js
+++ router_ex/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Content from './components/Content';
+import User from './components/User';
 function App() {
+  const [id, setId] = useState('admin');
   return (
     <Router>
       <div>
@@ -12,6 +14,7 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/content">Content</Link></li>
+            <li><Link to={"/user/"+id}>User</Link></li>
           </ul>
         </nav>
         <hr></hr>
@@ -19,6 +22,7 @@ function App() {
           <Route path='/' element={<Home/>}/>
           <Route path='/about' element={<About/>}/>
           <Route path='/content' element={<Content/>}/>
+          <Route path='/user/:id' element={<User/>}/>
         </Routes>
         <hr></hr>
       </div>
