1dbac979e8c31c3b7b6cfac40347386176f6f2c7
elzenaro <nam2626@naver.com>
2024-05-27 10:48:15 +0900

    redux

--- redux-ex-counter/src/component/Counter.js
+++ redux-ex-counter/src/component/Counter.js
@@ -1,13 +1,18 @@
-export default function Counter() {
+import { useDispatch, useSelector } from "react-redux";
+import { increament, decrement } from "../store/CounterSlice";
 
+export default function Counter() {
+  //상태값 선택
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
       <input type="text" />
       <button>증가</button>
       <hr/>
       <button>-</button>
-      <span></span>
-      <button>+</button>
+      <span>{count}</span>
+      <button onClick={()=>dispatch(increament())}>+</button>
     </div>
   );
 }
--- redux-ex-counter/src/store/store.js
+++ redux-ex-counter/src/store/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
-import { counterSlice } from './CounterSlice';
+import  counterSlice  from './CounterSlice';
 
 //빈 저장소 생성
 export default configureStore({
