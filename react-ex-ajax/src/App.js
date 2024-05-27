7115804538903529333e0fbe97537fe449f6a7d8
elzenaro <nam2626@naver.com>
2024-05-27 11:51:47 +0900

    axios

--- react-ex-ajax/src/App.js
+++ react-ex-ajax/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import OpenWeatherAjax from './components/OpenWeatherAjax';
+import OpenWeatherAxios from './components/OpenWeatherAxios';
 
 function App() {
   return (
     <div className="App">
       <OpenWeatherAjax/>
+      <hr></hr>
+      <OpenWeatherAxios/>
     </div>
   );
 }
