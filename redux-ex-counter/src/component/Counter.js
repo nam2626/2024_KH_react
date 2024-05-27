3168ba4d3ef20e62a6374349fa424db73008915e
elzenaro <nam2626@naver.com>
2024-05-27 10:24:19 +0900

    redux

---
+++ redux-ex-counter/src/component/Counter.js
@@ -0,0 +1,13 @@
+export default function Counter() {
+
+  return (
+    <div>
+      <input type="text" />
+      <button>증가</button>
+      <hr/>
+      <button>-</button>
+      <span></span>
+      <button>+</button>
+    </div>
+  );
+}
--- redux-ex-counter/src/store/store.js
+++ redux-ex-counter/src/store/store.js
@@ -1,6 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
+import { counterSlice } from './CounterSlice';
 
 //빈 저장소 생성
 export default configureStore({
-  reducer : {}
+  reducer : {
+    counter : counterSlice
+  }
 });
