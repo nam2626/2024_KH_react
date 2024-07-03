3f3255d71fbf74a5a74b1272cc037ffc35fc2a18
elzenaro <nam2626@naver.com>
2024-07-03 14:11:17 +0900

    Axios 호출

--- spring-front-app/src/components/Home.js
+++ spring-front-app/src/components/Home.js
@@ -1,12 +1,16 @@
 import { useEffect } from "react"
+import axios from 'axios'
 import '../css/Home.css'
 export default function Home() {
   //액시오스로 전체 회원 데이터를 가져오는 코드
   useEffect(() => {
     //http://localhost:9999/member/list
     //아이디 이름 닉네임 회원등급
-
-  });
+    axios.get('http://localhost:9999/member/list')
+    .then(response => {
+      console.log(response);
+    })
+  },[]);
   return (
     <>
       <h2>회원 목록</h2>
