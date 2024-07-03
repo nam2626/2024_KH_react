7142a4b0ce8bc76899568335da82bb9f3e33444e
elzenaro <nam2626@naver.com>
2024-07-03 13:37:31 +0900

    기본 컴포넌트 작성

--- spring-front-app/src/App.js
+++ spring-front-app/src/App.js
@@ -1,12 +1,15 @@
-import {BrowserRouter as Router} from 'react-router-dom';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-
-      <hr/>
       
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Home/>} />        
+      </Routes>
     </Router>
   );
 }
---
+++ spring-front-app/src/components/Home.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  //액시오스로 전체 회원 데이터를 가져오는 코드
+  
+  return (
+    <>
+      <h2>회원 목록</h2>
+      <hr></hr>
+    </>
+  )
+}
