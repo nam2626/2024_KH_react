61c769f1b64891175b40c75d358f4261c6dcfacf
elzenaro <nam2626@naver.com>
2024-07-03 15:03:59 +0900

    회원 목록 출력

--- spring-front-app/src/components/Home.js
+++ spring-front-app/src/components/Home.js
@@ -1,21 +1,49 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from 'axios'
 import '../css/Home.css'
 export default function Home() {
+  let [memberList, setMemberList] = useState([]);
+
   //액시오스로 전체 회원 데이터를 가져오는 코드
   useEffect(() => {
     //http://localhost:9999/member/list
     //아이디 이름 닉네임 회원등급
-    axios.get('http://localhost:9999/member/list')
-    .then(response => {
-      console.log(response);
-    })
-  },[]);
-  return (
+    const readData = async () => {
+        axios.get('http://localhost:9999/member/list')
+        .then(response => {
+          console.log(response);
+          setMemberList((v) => [...response.data]);
+        })
+      }
+      readData();
+    },[]);
+    console.log(memberList);
+
+    return (
     <>
       <h2>회원 목록</h2>
-      <hr></hr>
-
+      <table>
+        <thead>
+          <th>아이디</th>
+          <th>이름</th>
+          <th>닉네임</th>
+          <th>등급</th>
+        </thead>
+        <tbody>
+        {
+          memberList.map((item,idx) => {
+            return (
+              <tr key={idx}>
+                <td>{item.boardMemberId}</td>
+                <td>{item.boardMemberName}</td>
+                <td>{item.boardMemberNick}</td>
+                <td>{item.boardMemberGrade}</td>
+              </tr>
+            );
+          })  
+        }
+        </tbody>
+      </table>
     </>
   )
 }
