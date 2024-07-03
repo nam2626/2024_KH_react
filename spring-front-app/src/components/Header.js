2c0778dbc84339d5a808a823ac46e35f9782e811
elzenaro <nam2626@naver.com>
2024-07-03 13:47:43 +0900

    css 추가

--- spring-front-app/src/components/Header.js
+++ spring-front-app/src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import '../css/Header.css';
 export default function Header() {
   return (
     <>
--- spring-front-app/src/components/Home.js
+++ spring-front-app/src/components/Home.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react"
-
+import '../css/Home.css'
 export default function Home() {
   //액시오스로 전체 회원 데이터를 가져오는 코드
   useEffect(() => {
     //http://localhost:9999/member/list
     //아이디 이름 닉네임 회원등급
-    
+
   });
   return (
     <>
