bc85f5d97d2014e07f1523ffdbfce8a7339c29b0
elzenaro <nam2626@naver.com>
2024-07-04 15:04:13 +0900

    데이터 읽어오는 부분

--- spring-front-app/src/components/MemberView.js
+++ spring-front-app/src/components/MemberView.js
@@ -1,8 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MemberView() {
   const {memberId} = useParams();
   console.log(memberId);
+  const [member, setMember] = useState();
+  useEffect(()=>{
+    const fetchData = () => {
+      axios.get('http://localhost:9999/member/'+memberId)
+      .then(response => {
+          console.log(response);
+      });
+    }
+    fetchData();
+  },[]);
+  if(member == null){
+    return <div>회원 데이터를 로딩중입니다.</div>
+  }
   return (
     <div>{memberId}</div>
   ); 
